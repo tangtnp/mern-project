0a8aa5a112193531db9b5eb277257c29552c3fae
Tanathorn Piyachart <tangutang.tnp@gmail.com>
2024-01-08 15:21:22 +0700

    src

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>MERN Stack | Workshop</h1>
+      <button className="btn btn-primary">Bootstrap</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/FromComponent.js
@@ -0,0 +1,8 @@
+const FormComponent=()=>{
+    return (
+        <div className="container">
+          <h1>From component</h1>
+        </div>
+      );
+}
+export default FormComponent;
